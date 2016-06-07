3e3e82545e752d92de1cfead8c39dd52c07bfcde
baron <ike960705@gmail.com>
2016-06-07 13:35:32 +0800

    移除文件

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
-from app import app
+from app import app, db
+from flask_script import Manager, Server
+
+manager = Manager(app)
+manager.add_command("runserver", Server(host="0.0.0.0", port="3000" ))
+
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    manager.run()
