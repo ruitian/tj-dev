4bf7e488234740fdb7ef09c4d3551116241d797b
baron <ike960705@gmail.com>
2016-06-12 14:56:49 +0800

    增加字段

--- app/models/server.py
+++ app/models/server.py
@@ -5,5 +5,10 @@ from app import db
 class ServerModel(db.Model):
     __tablename__ = 'server'
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(256), nullable=False)
-    Ip = db.Column(db.String(64), nullable=False)
+    nickname = db.Column(db.String(256), nullable=False, index=True)
+    Ip = db.Column(db.String(64), nullable=False, index=True)
+    create_on = db.Column(
+        db.TIMESTAMP,
+        index=True,
+        server_default=db.func.current_timestamp()
+    )
