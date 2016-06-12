f6cc1ded5f20ee06c2c2f351220655ad9a413b95
baron <ike960705@gmail.com>
2016-06-12 14:58:18 +0800

    添加主机和显示主机

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, Regexp
+from wtforms.validators import Required, Email, Length, Regexp
 
 
 class LoginForm(Form):
-    username_or_email = StringField(validators=[DataRequired()])
-    password = PasswordField(validators=[DataRequired()])
+    username_or_email = StringField(validators=[Required()])
+    password = PasswordField(validators=[Required()])
     submit = SubmitField(u'登录')
 
 
 class RegisterForm(Form):
-    email = StringField(validators=[DataRequired(), Email()])
+    email = StringField(validators=[Required(), Email()])
     username = StringField(validators=[
-        DataRequired(),
+        Required(),
         Length(3, 20),
         Regexp('[A-Za-z][A-Za-z0-9_.]*$', 0,
                'Nickname must have only letter, '
                'numbers, dots and underscores')
     ])
-    password = PasswordField(validators=[DataRequired()])
+    password = PasswordField(validators=[Required()])
     submit = SubmitField(u'添加')
+
+
+class ServerForm(Form):
+    nickname = StringField(validators=[Required()])
+    ip = StringField(validators=[Required()])
+    submit = SubmitField(u'提交')
--- app/views/server.py
+++ app/views/server.py
@@ -1,8 +1,35 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import \
+    (
+        render_template,
+        request,
+        flash,
+        redirect,
+        url_for
+    )
 from flask_socketio import send, emit
+from flask_login import login_required
 
-from app import app, socketio
+from app import app, socketio, db
+from app.forms import ServerForm
+from app.models import ServerModel
+
+
+class ServerService(object):
+
+    @staticmethod
+    def get_server_by_nickname(nickname):
+        return ServerModel.query.filter_by(nickname=nickname).first()
+
+    @staticmethod
+    def get_server_by_ip(ip):
+        return ServerModel.query.filter_by(Ip=ip).first()
+
+    @staticmethod
+    def get_servers(page):
+        return ServerModel.query.order_by(
+            ServerModel.create_on.desc()).paginate(
+                page, app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
 
 
 @app.route('/test')
@@ -21,5 +48,24 @@ def handle_my_custom_event(json):
 
 
 @app.route('/server', methods=['GET', 'POST'])
+@login_required
 def add_server():
-    return render_template('server.html')
+    form = ServerForm()
+    if request.method == 'POST' and form.validate_on_submit:
+        nickname = form.nickname.data
+        ip = form.ip.data
+        if ServerService.get_server_by_nickname(nickname) is not None:
+            flash(u'添加失败,名称已被占用', 'danger')
+            redirect(url_for('add_server'))
+        elif ServerService.get_server_by_ip(ip) is not None:
+            flash(u'添加失败,该主机IP已被添加', 'danger')
+            redirect(url_for('add_server'))
+        else:
+            server = ServerModel(nickname=nickname, Ip=ip)
+            db.session.add(server)
+            db.session.commit()
+            flash(u'添加成功', 'success')
+    page = request.args.get('page', 1, type=int)
+    pagination = ServerService.get_servers(page)
+    servers = pagination.items
+    return render_template('server.html', form=form, servers=servers, pagination=pagination)
