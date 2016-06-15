cd3ebcb031e0240a977e397f1e2b841d9a80d376
baron <ike960705@gmail.com>
2016-06-15 18:35:47 +0800

    回去继续写git这一块

--- app/views/build.py
+++ app/views/build.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
+import json
 from flask_login import login_required, current_user
 from flask import render_template
 
-from app import app, oauth
-from app.utils import GithubOAuth
+from app import app, redis
 
 
 @app.route('/build')
@@ -15,5 +15,14 @@ def build_code():
 @app.route('/build/new')
 @login_required
 def build_code_new():
-    print GithubOAuth.github.get('user').data
-    return render_template('build-new.html')
+    github_data = json.loads(redis.hget(current_user.id, 'github_data'))
+    github_orgs_data = redis.hget(current_user.id, 'github_orgs_data')
+    if github_orgs_data is not None:
+        github_orgs_data = eval(github_orgs_data)
+
+    # github_avatar = github_data['avatar_url']
+    return render_template(
+        'build-new.html',
+        github_data=github_data,
+        github_orgs_data=github_orgs_data
+        )
