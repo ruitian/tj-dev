3dd961bf9cabcbbef3a2a0fad13d13b8b17c02a4
baron <ike960705@gmail.com>
2016-06-20 13:39:27 +0800

    增加gitlab_token字段

--- app/models/user.py
+++ app/models/user.py
@@ -22,6 +22,7 @@ class UserModel(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(256))
     github_token = db.Column(db.String(256), default=None)
+    gitlab_token = db.Column(db.String(256), default=None)
     # 用户角色
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
