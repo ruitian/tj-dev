dc1e70a29835d6b9ba796fbbc2c30adf489e5698
ruitian <chinabingwei@163.com>
2016-06-22 00:12:10 +0800

    增加项目的确认值

--- app/models/project.py
+++ app/models/project.py
@@ -7,6 +7,7 @@ class ProjectModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     proname = db.Column(db.String(256), nullable=False, index=True)
     address = db.Column(db.String(256), nullable=False, index=True)
+    verify = db.Column(db.String(256), nullable=False, index=True)
     create_on = db.Column(
         db.TIMESTAMP,
         index=True,
