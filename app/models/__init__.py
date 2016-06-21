15d262d0a4449d5e52a45b53420495ac352b3aa4
屋檐下听雨 <ike960705@gmail.com>
2016-06-21 16:57:02 +0800

    创建项目时的数据表

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@
 from .user import *  # noqa
 from .role import *  # noqa
 from .server import *  # noqa
+from .project import *  # noqa
---
+++ app/models/project.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from app import db
+
+
+class ProjectModel(db.Model):
+    __tablename__ = 'project'
+    id = db.Column(db.Integer, primary_key=True)
+    proname = db.Column(db.String(256), nullable=False, index=True)
+    address = db.Column(db.String(256), nullable=False, index=True)
+    create_on = db.Column(
+        db.TIMESTAMP,
+        index=True,
+        server_default=db.func.current_timestamp()
+    )
