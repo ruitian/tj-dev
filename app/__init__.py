0d86c417d9290e902ca6d00b844ea1fa577f59fc
屋檐下听雨 <ike960705@gmail.com>
2016-06-22 21:58:03 +0800

    add eventlet

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,37 @@ from flask_moment import Moment
 
 from .config import config
 
+async_mode = None
+
+if async_mode is None:
+    try:
+        import eventlet
+        async_mode = 'eventlet'
+    except ImportError:
+        pass
+
+    if async_mode is None:
+        try:
+            from gevent import monkey
+            async_mode = 'gevent'
+        except ImportError:
+            pass
+
+    if async_mode is None:
+        async_mode = 'threading'
+
+    print('async_mode is ' + async_mode)
+
+# monkey patching is necessary because this application uses a background
+# thread
+if async_mode == 'eventlet':
+    import eventlet
+    eventlet.monkey_patch()
+elif async_mode == 'gevent':
+    from gevent import monkey
+    monkey.patch_all()
+
+
 app = Flask(__name__)
 login_manager = LoginManager()
 db = SQLAlchemy()
@@ -33,7 +64,7 @@ with app.app_context():
     login_manager.login_message = u'请先登陆系统,若遗忘密码，请联系管理员'
     login_manager.login_message_category = 'warning'
 
-    socketio.init_app(app)
+    socketio.init_app(app, async_mode=async_mode)
     redis.init_app(app)
     moment.init_app(app)
 
