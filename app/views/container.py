cd15615d0c67ba58cb46941bdfb5aba6c0107fe1
屋檐下听雨 <ike960705@gmail.com>
2016-07-01 16:53:13 +0800

    创建应用

--- app/views/container.py
+++ app/views/container.py
@@ -3,17 +3,23 @@ import os
 import json
 from flask import render_template
 from flask import request
-from flask import redirect
-from flask import url_for
-from flask import flash
 from flask_login import login_required
 from docker import Client
 
 from app import app
 from app import db
+from app import socketio
+from app import redis
 from app.models import ProjectModel
 from app.models import ServerModel
 from app.models import AppModel
+
+REGISTRY_IP = '172.16.6.130:5000/'
+APPS = None
+APP_NAME = None
+HOST = None
+IMAGE = None
+PORT = None
 
 
 @app.route('/apps')
@@ -44,52 +50,86 @@ def create_app(verify):
 @app.route('/apps/per/<verify>', methods=['GET', 'POST'])
 def per_app(verify):
     apps = AppModel.query.filter_by(verify=verify).first()
+    global APPS
+    global APP_NAME
+    global HOST
+    global IMAGE
+    global PORT
     if request.method == 'POST' and apps is None:
-        appname = request.form['appname']
-        ip = request.form['server']
-        port = request.form['port']
-        image = request.form['image']
-
-        try:
-            cli = Client(base_url=ip+':5678')
-            container = cli.create_container(
-                image=image, ports=[port], name=appname,
-                host_config=cli.create_host_config(port_bindings={
-                    3000: (ip, port)
-                })
-            )
-            apps = AppModel(
-                appname=appname,
-                port=port,
-                host=ip,
-                image=image,
-                verify=verify,
-                containerId=container.get('Id')
-            )
-            db.session.add(apps)
-            db.session.commit()
-
-            response = cli.start(container=container.get('Id'))
-            if response is None:
-                return redirect(url_for('per_app', verify=verify))
-        except:
-            flash(u'未知错误', category='danger')
-            return redirect(url_for('apps'))
+        APP_NAME = appname = request.form['appname']
+        HOST = ip = request.form['server']
+        PORT = port = request.form['port']
+        IMAGE = image = REGISTRY_IP + request.form['image']
+        APPS = apps = AppModel(
+            appname=appname,
+            port=port,
+            host=ip,
+            image=image,
+            verify=verify,
+        )
+        db.session.add(apps)
+        db.session.commit()
+        return render_template('app.html', apps=apps)
+    elif apps is not None:
+        APPS = apps
+        APP_NAME = apps.appname
+        HOST = apps.host
+        IMAGE = apps.image
+        PORT = apps.port
+    elif apps is None:
+        apps = APPS
     return render_template('app.html', apps=apps)
+
+
+@app.route('/apps/create', methods=['POST'])
+def create_app_socket():
+    logs = []
+    status = json.loads(container_status())['status']
+    # 如果容器还没有,就开始创建
+    if status == 'pending':
+        cli = Client(base_url=HOST+':5678')
+        for line in cli.pull(IMAGE, stream=True):
+            socketio.emit('response', json.dumps({'resp': line}))
+            logs.append(line)
+        # 日志存入缓存
+        redis.hset(APP_NAME, 'app_logs', logs)
+        container = cli.create_container(
+            image=IMAGE, ports=[PORT], name=APP_NAME,
+            host_config=cli.create_host_config(port_bindings={
+                3000: (HOST, PORT)
+            })
+        )
+        APPS.containerId = container.get('Id')
+        db.session.add(APPS)
+        db.session.commit()
+        response = cli.start(container=container.get('Id'))
+        if response is not None:
+            return json.dumps({'msg': 'false'})
+    # 在缓存中读取日志
+    else:
+        for line in eval(redis.hget(APP_NAME, 'app_logs')):
+            socketio.emit('response', json.dumps({'resp': line}))
+    return json.dumps({'msg': 'ok'})
 
 
 @app.route('/apps/per/status', methods=['POST'])
 def container_status():
-    names = []
-    data = json.loads(request.get_data())
-    cli = Client(base_url=data['host']+':5678')
-    containers = cli.containers()
-    for container in containers:
-        names.append(container['Names'][0].split('/')[1])
-    if data['name'] in names:
+    run_names = []
+    all_names = []
+    cli = Client(base_url=str(HOST)+':5678')
+    run_containers = cli.containers()
+    all_containers = cli.containers(all=True)
+    for container in run_containers:
+        run_names.append(container['Names'][0].split('/')[1])
+    for container in all_containers:
+        all_names.append(container['Names'][0].split('/')[1])
+    if APP_NAME in run_names:
         status = 'up'
     else:
-        status = 'down'
+        if APP_NAME in all_names:
+            status = 'down'
+        else:
+            status = 'pending'
     return json.dumps({'status': status})
 
 
