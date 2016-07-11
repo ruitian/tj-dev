0791ee4e08f12082f83a3a6a8393496f977a1440
屋檐下听雨 <ike960705@gmail.com>
2016-07-11 22:52:51 +0800

    修改密码功能，管理员功能

--- app/views/user.py
+++ app/views/user.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+import json
 from app import app, db
 from app.forms import LoginForm, RegisterForm
 from app.models import UserModel
 
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
 
 class UserService(object):
@@ -78,3 +79,57 @@ def login():
         flash(u'用户名或密码错误', 'danger')
         return redirect(url_for('login'))
     return render_template("login.html", form=form)
+
+
+@app.route("/accounts/profile", methods=['GET', 'POST'])
+@login_required
+def user_profile():
+    if current_user.is_administrator():
+        users = UserModel.query.order_by(UserModel.id.desc())
+        return render_template('admin.html', users=users)
+    else:
+        return render_template('profile.html')
+
+
+@app.route("/accounts/profile/setting", methods=['GET', 'POST'])
+@login_required
+def user_profile_setting():
+    username = request.form['username']
+    password = request.form['password']
+    user = UserModel.query.filter_by(username=username).first()
+    user.password = password
+    db.session.add(user)
+    db.session.commit()
+    flash(u'修改成功!', 'success')
+    return redirect(url_for('user_profile'))
+
+
+@app.route('/accounts/search', methods=['POST'])
+@login_required
+def search_user():
+    username = json.loads(request.get_data())['data']
+    user = UserModel.query.filter_by(username=username).first()
+    if user is None:
+        return json.dumps({'msg': 0})
+    else:
+        return json.dumps({'msg': 1, 'username': user.username, 'email': user.email})
+
+
+@app.route('/accounts/profile/modify', methods=['GET', 'POST'])
+@login_required
+def user_profile_modify():
+    print 123
+    username = request.form['username']
+    old_pass = request.form['old-password']
+    new_pass = request.form['new-password']
+    user = UserModel.query.filter_by(username=username).first()
+    if not user.verify_password(old_pass):
+        flash(u'原密码不匹配', 'danger')
+        return redirect(url_for('user_profile'))
+    else:
+        user.password = new_pass
+        db.session.add(user)
+        db.session.commit()
+        flash(u'修改成功', 'success')
+        return redirect(url_for('user_profile'))
+
