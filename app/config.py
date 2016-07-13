2d1640952f8a8f7c01d3b4481e77ec6872680f9f
Dong Bingwei <dongbingwei@innobuddy.com>
2016-07-13 09:08:03 +0000

    Update

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     FLASKY_POSTS_PER_PAGE = 5
     CODE_FOLDER = BASE_DIR + '/code'
-    HOST = '172.16.3.9'
+    HOST = '172.16.3.9:5678'
     REGISTRY = '172.16.3.9:5000/'
 
     @staticmethod
